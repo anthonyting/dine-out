df8fd27812357cebee33a64e3500cdbda276ba82
Anthony Ting <anthonydting@gmail.com>
2025-01-21 23:38:57 -0800

    add netlify cache

---
+++ plugins/netlify-cache-dist/index.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const scrapingDistPath = path.resolve(
+  __dirname,
+  "../../packages/scraping/dist"
+);
+
+module.exports = {
+  async onPreBuild({ utils }) {
+    await utils.cache.restore();
+
+    const files = await utils.cache.list({ depth: 10 });
+    console.log("Cached files", files);
+  },
+  async onPostBuild({ utils }) {
+    await utils.cache.save(scrapingDistPath);
+
+    const files = await utils.cache.list({ depth: 10 });
+    console.log("Cached files", files);
+  },
+};
